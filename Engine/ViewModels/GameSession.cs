14ba7c19e0e21f70fda4f090993fa1fb25dc4e97
Andre Bouchard <EllumEnohpee@gmail.com>
2019-09-17 00:57:19 -0700

    Added movement controls

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -4,16 +4,45 @@ using System.Text;
 using Engine.Models;
 using Engine;
 using Engine.Factories;
+using System.ComponentModel;
 
 namespace Engine.ViewModels
 {
-    public class GameSession
+    public class GameSession : INotifyPropertyChanged
     {
+        //Properties
+        private Location _currentLocation;
         public Player CurrentPlayer { get; set; }
-        public Location CurrentLocation { get; set; }
+        public Location CurrentLocation
+        {
+            get { return _currentLocation; }
+            set
+            {
+                _currentLocation = value;
+                OnPropertyChanged("CurrentLocation");
+                OnPropertyChanged("HasLocationToNorth");
+                OnPropertyChanged("HasLocationToSouth");
+                OnPropertyChanged("HasLocationToEast");
+                OnPropertyChanged("HasLocationToWest");
+            }
+        }
         public World CurrentWorld { get; set; }
-        
-
+        public bool HasLocationToNorth
+        {
+            get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1) != null; }
+        }
+        public bool HasLocationToSouth
+        {
+            get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1) != null; }
+        }
+        public bool HasLocationToEast
+        {
+            get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate) != null; }
+        }
+        public bool HasLocationToWest
+        {
+            get { return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null; }
+        }
         //Constructors
         public GameSession()
         {
@@ -31,18 +60,39 @@ namespace Engine.ViewModels
             CurrentWorld = Factory.CreateWorld();
             CurrentLocation = CurrentWorld.LocationAt(0, 0);
 
-
-            //Initialize starting Location info
-            /*
-            CurrentLocation = new Location();
-            CurrentLocation.XCoordinate = 0;
-            CurrentLocation.YCoordinate = 1;
-            CurrentLocation.Name = "Home";
-            CurrentLocation.Description = "This is your House";
-            CurrentLocation.ImageName = "Locations/Home.png";
-            */
-
+                       
         }
 
+        //Methods
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
+
+        //Event Handling
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+            
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -34,5 +34,23 @@ namespace WPFUI
         {
             _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
         }
+
+        private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveNorth();
+        }
+        private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveSouth();
+        }
+        private void OnClick_MoveEast(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveEast();
+        }
+        private void OnClick_MoveWest(object sender, RoutedEventArgs e)
+        {
+            _gameSession.MoveWest();
+        }
+
     }
 }
